a067f174507fb7226154129cbec2b65d97cdf38f
isaacv3 <iv2050@nyu.edu>
2025-04-07 18:51:10 -0400

    fixed errors

--- web-app/app.py
+++ web-app/app.py
@@ -4,6 +4,7 @@ Web Application for Microphone Translation.
 This Flask app connects to a MongoDB database and handles routes 
 to interact with the microphone translation app.
 """
+
 import os
 from flask import Flask
 from flask_pymongo import PyMongo
@@ -15,11 +16,12 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 mongo = PyMongo(app)
 
+
 @app.route("/")
 def home():
     """Home page for the web app."""
     return "<h1>hello<h1>"
 
+
 if __name__ == "__main__":
-    """Start the web app."""
     app.run(host="0.0.0.0", port=5050)
